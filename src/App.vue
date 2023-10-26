2c04e2d9d378bfd093058d6b375e940a808a5732
Khant-Nyar-Ko-Ko <kokhantnyar19@gmail.com>
2023-10-27 01:58:34 +0700

    toggle

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,40 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
-    <Tasks :tasks = "tasks"/>
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
   </div>
 </template>
 
 <script>
-import Header from "./components/Header";
-import Tasks from "./components/Tasks";
-
+import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks.vue";
 
 export default {
   name: "App",
   components: {
     Header,
-    Tasks
+    Tasks,
   },
   data() {
     return {
       tasks: [],
     };
+  },
+  methods: {
+    deleteTask(id) {
+      if (confirm("Are you sure?")) {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
   },
   created() {
     this.tasks = [
@@ -39,7 +54,7 @@ export default {
         id: 3,
         text: "Mens Cotton Jacket",
         day: 55.99,
-        reminder: true,
+        reminder: false,
       },
     ];
   },
